d80e986da36d0e89e07eed8879129d73ac1fef05
Kodam-Vinay <vinay.kodam112@gmail.com>
2024-02-08 17:28:34 +0530

    changed some config in package.json file

--- src/config/constants.js
+++ src/config/constants.js
@@ -7,7 +7,7 @@ export const YOUTUBE_SEARCH_SUGGESTIONS_API =
 export const YOUTUBE_VIDEO_DETAILS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`;
 export const SEARCH_RESULTS_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=vinay&maxResults=20&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`;
 export const YOUTUBE_VIDEO_PLAYER_URL =
-  "crossproxy.io?/https://www.youtube.com/embed/";
+  "corsproxy.io?/https://www.youtube.com/embed/";
 export const YOUTUBE_VIDEO_DETAILS_WITH_ONE_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc%2Cc0KYU2j0TM4%2CeIho2S0ZahI&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`;
 export const CLOUDINARY_IMAGE_URL =
   "https://res.cloudinary.com/dwgpba5n2/image/upload/v1694021773/videoStreaming/";
